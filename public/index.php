f61510c46df7da6f46711dba936f99051b715fe8
Sergey Sinicin <sergey@sinicin.lv>
2020-10-12 16:57:57 +0300

    check config.php

--- public/index.php
+++ public/index.php
@@ -3,8 +3,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require __DIR__.'/../vendor/autoload.php';
+if (!is_file(__DIR__.'/../config.php')) {
+    die('rename config.php.example to config.php');
+}
+
 require_once '../config.php';
+require __DIR__.'/../vendor/autoload.php';
 require_once '../app/system/helper/functions.php';
 
 $app = new App\System\Library\App();
